dc3361cf782647591cf6dfc1e38cb62b2a26e0e2
GiovanniNoda <giovanniminoru@gmail.com>
2024-06-24 15:04:57 -0300

    novos ex

---
+++ javascript/exercicios/ex001/script.js
@@ -0,0 +1,37 @@
+//let weight = 
+//console.log(typeof weight)
+
+/*
+let name = 'Giovanni'
+let age = 25
+let stars = 12.5
+let isSubscribed = true
+*/
+
+
+const student = {
+    name: 'Giovanni',
+    age: 25,
+    weight: 99.5,
+    isSubscrided: true
+}
+
+//console.log(student.name + ' de idade '  + student.age + ' pesa ' + student.weight)
+
+
+let students = []
+
+students = [
+    student
+]
+//console.log(students[0])
+
+const john = {
+    name: 'John',
+    age: 21,
+    weight: 74.2,
+    isSubscrided: true
+}
+students[1] = john
+
+console.log(students)
