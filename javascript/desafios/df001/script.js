1bdad33829a68964ad92472c1d4fe2aa8357d173
GiovanniNoda <giovanniminoru@gmail.com>
2024-07-01 17:38:48 -0300

    hgfd

---
+++ javascript/desafios/df001/script.js
@@ -0,0 +1,15 @@
+// solicitar dois números ao usuário
+
+let number1 = Number(prompt("Insira o primeiro número:"))
+let number2 = Number(prompt("Insira o segundo número:"))
+
+let numbers = [number1, number2]
+
+checkNumber()
+
+function checkNumber() {
+    for (i = 0; i < numbers.length; i++) {
+        
+    }
+        
+}
